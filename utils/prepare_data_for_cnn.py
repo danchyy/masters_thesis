d091957152706908ebe37eb2dda7c5773e1ee9a7
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-15 17:53:39 +0200

    Added script which prepares data for CNN

---
+++ utils/prepare_data_for_cnn.py
@@ -0,0 +1,54 @@
+from utils import constants
+import os
+from shutil import copyfile
+
+
+def create_train_test_dict(train_split_lines, test_split_lines):
+    train_test_dict = dict()
+    for line in train_split_lines:
+        line = line.split(" ")[0]
+        video_name = line.split("/")[1].split(".")[0]
+        train_test_dict[video_name] = "TRAIN"
+    for line in test_split_lines:
+        video_name = line.split("/")[1].split(".")[0]
+        train_test_dict[video_name] = "TEST"
+
+    return train_test_dict
+
+
+target_dir = constants.UCF_101_CNN_DATA_DIR
+source_dir = constants.UCF_101_FRAMES_DIR
+
+train_split = open(os.path.join(constants.UCF_101_TRAIN_TEST_SPLIT_CLASS_DIR, "cleaned_train01.txt"), "r").readlines()
+
+test_split = open(os.path.join(constants.UCF_101_TRAIN_TEST_SPLIT_CLASS_DIR, "cleaned_test01.txt"), "r").readlines()
+
+train_test_dict = create_train_test_dict(train_split, test_split)
+
+for class_name in os.listdir(source_dir):
+    if class_name.startswith("."):
+        continue
+    class_path = os.path.join(source_dir, class_name)
+    video_names = os.listdir(class_path)
+    train_dest_path = os.path.join(target_dir, "train", class_name)
+    test_dest_path = os.path.join(target_dir, "validation", class_name)
+    if not os.path.exists(train_dest_path):
+        os.makedirs(train_dest_path)
+        os.makedirs(test_dest_path)
+    for video_name in video_names:
+        if video_name.startswith("."):
+            continue
+        video_name_path = os.path.join(class_path, video_name)
+        frame_in_middle = len(os.listdir(video_name_path)) // 2
+        frame_path = os.path.join(video_name_path, "frame_" + str(frame_in_middle) + ".jpg")
+        if video_name not in train_test_dict:
+            continue
+        if train_test_dict[video_name] == "TRAIN":
+            copyfile(frame_path,
+                     os.path.join(target_dir, "train", class_name, video_name + "frame_" + str(frame_in_middle)
+                                  + ".jpg"))
+        elif train_test_dict[video_name] == "TEST":
+            if train_test_dict[video_name] == "TRAIN":
+                copyfile(frame_path,
+                         os.path.join(target_dir, "validation", class_name, video_name + "_frame_" + str(frame_in_middle)
+                                      + ".jpg"))
