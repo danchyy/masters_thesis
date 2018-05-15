21470fd7576a1afdf2d7cab53d75bb3400d5cf13
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-15 17:52:37 +0200

    Renamed utils.py script

--- utils/util_script.py
+++ utils/util_script.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from utils import constants
+import constants
 
 
 def get_args():
@@ -17,9 +17,9 @@ def get_args():
 
 def get_ucf_101_dict():
     class_lines = open(os.path.join(constants.UCF_101_TRAIN_TEST_SPLIT_CLASS_DIR,
-                                    constants.UCF_101_SPLIT_FILE_NAME)).readlines()
+                                    constants.UCF_101_CLASS_FILE_NAME)).readlines()
     class_dict = dict()
     for line in class_lines:
         splitted_line = line.split(" ")
-        class_dict[splitted_line[1]] = int(splitted_line[0])
+        class_dict[splitted_line[1].strip()] = int(splitted_line[0])
     return class_dict
