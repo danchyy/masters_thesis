17babce85b95a8ef0b26d1b17da2a7128e1888dc
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-16 23:54:23 +0200

    Added building of model for fine tuned cnn

--- models/fine_tuned_cnn.py
+++ models/fine_tuned_cnn.py
@@ -3,6 +3,7 @@ from keras import applications
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Dropout, GlobalAveragePooling2D
 from utils.util_script import get_number_of_classes
+from utils import constants
 from keras import optimizers, losses
 
 class FineTunedCNN(BaseModel):
@@ -12,12 +13,17 @@ class FineTunedCNN(BaseModel):
 
     def build_model(self):
         self.model = Sequential()
-        pretrained_model = applications.ResNet50(include_top=False, weights="imagenet")
+        pretrained_model = applications.ResNet50(include_top=False, weights="imagenet",
+                                                 input_shape=(constants.RESNET_DIMS[0], constants.RESNET_DIMS[1], 3))
+        for layer in pretrained_model.layers:
+            layer.trainable = False
         self.model.add(pretrained_model)
+        self.model.add(Dense(512, activation="relu"))
         self.model.add(Dense(256, activation="relu"))
         self.model.add(Dense(512, activation="relu"))
         self.model.add(Dropout(0.5))
         self.model.add(Dense(get_number_of_classes(), activation='softmax'))
-        self.model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy", "precision",
-                                                                                       "accuracy"])
+        optimizer = optimizers.get(self.config.model)
+        self.model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy", "precision",
+                                                                                          "recall"])
         return self.model
