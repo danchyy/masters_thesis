31e5f11efebaf68dd75b4f35bae3272b33ea90d4
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-26 00:03:39 +0200

    Changed format of checkpoint and log files

--- utils/config.py
+++ utils/config.py
@@ -22,8 +22,8 @@ def get_config_from_json(json_file):
 
 def process_config(json_file):
     config, _ = get_config_from_json(json_file)
-    config.callbacks.tensorboard_log_dir = os.path.join("experiments", time.strftime("%Y-%m-%d/", time.localtime()),
-                                                        config.exp.name, "logs/")
-    config.callbacks.checkpoint_dir = os.path.join("experiments", time.strftime("%Y-%m-%d/", time.localtime()),
+    config.callbacks.tensorboard_log_dir = os.path.join("experiments", time.strftime("%Y-%m-%d-%H-%M-%S/",
+                                                        time.localtime()), config.exp.name, "logs/")
+    config.callbacks.checkpoint_dir = os.path.join("experiments", time.strftime("%Y-%m-%d-%H-%M-%S/", time.localtime()),
                                                    config.exp.name, "checkpoints/")
     return config
