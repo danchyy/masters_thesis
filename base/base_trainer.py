b02ba066f1c71a8bb93f5b60a828de5eefa0f0b3
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-31 13:26:13 +0200

    Changed init callbacks method to be in base class

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -1,3 +1,8 @@
+import os
+
+from keras.callbacks import ModelCheckpoint, TensorBoard
+
+
 class BaseTrain(object):
     def __init__(self, model, data, config):
         self.model = model
@@ -6,3 +11,24 @@ class BaseTrain(object):
 
     def train(self):
         raise NotImplementedError
+
+    def init_callbacks(self):
+        self.callbacks.append(
+            ModelCheckpoint(
+                filepath=os.path.join(self.config.callbacks.checkpoint_dir,
+                                      '%s-{epoch:02d}-{val_loss:.2f}.hdf5' % self.config.exp.name),
+                monitor=self.config.callbacks.checkpoint_monitor,
+                mode=self.config.callbacks.checkpoint_mode,
+                save_best_only=self.config.callbacks.checkpoint_save_best_only,
+                save_weights_only=self.config.callbacks.checkpoint_save_weights_only,
+                verbose=self.config.callbacks.checkpoint_verbose,
+                period=10
+            )
+        )
+
+        self.callbacks.append(
+            TensorBoard(
+                log_dir=self.config.callbacks.tensorboard_log_dir,
+                write_graph=self.config.callbacks.tensorboard_write_graph,
+            ))
+
--- trainers/fine_tuned_trainer.py
+++ trainers/fine_tuned_trainer.py
@@ -19,25 +19,6 @@ class FineTunedTrainer(BaseTrain):
         self.train_split = self.config.trainer.train_split
         self.test_split = self.config.trainer.test_split
 
-    def init_callbacks(self):
-        self.callbacks.append(
-            ModelCheckpoint(
-                filepath=os.path.join(self.config.callbacks.checkpoint_dir,
-                                      '%s-{epoch:02d}-{val_loss:.2f}.hdf5' % self.config.exp.name),
-                monitor=self.config.callbacks.checkpoint_monitor,
-                mode=self.config.callbacks.checkpoint_mode,
-                save_best_only=self.config.callbacks.checkpoint_save_best_only,
-                save_weights_only=self.config.callbacks.checkpoint_save_weights_only,
-                verbose=self.config.callbacks.checkpoint_verbose,
-            )
-        )
-
-        self.callbacks.append(
-            TensorBoard(
-                log_dir=self.config.callbacks.tensorboard_log_dir,
-                write_graph=self.config.callbacks.tensorboard_write_graph,
-            ))
-
     def train(self):
         steps_per_epoch = get_number_of_items(self.train_split) // self.config.trainer.batch_size
         validation_steps = get_number_of_items(self.test_split) // self.config.trainer.batch_size
