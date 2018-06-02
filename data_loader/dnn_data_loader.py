b808b518f4526b16589bd03763b508a4bacfb500
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-02 22:21:07 +0200

    Changed data loader and constants

--- data_loader/dnn_data_loader.py
+++ data_loader/dnn_data_loader.py
@@ -8,8 +8,8 @@ class DNNDataLoader(BaseDataLoader):
     def __init__(self, config):
         super().__init__(config)
         self.batch_size = self.config.trainer.batch_size
-        self.train_dir = os.path.join(constants.UCF_101_LSTM_DATA_train01, "train")
-        self.test_dir = os.path.join(constants.UCF_101_LSTM_DATA_train01, "test")
+        self.train_dir = os.path.join(constants.UCF_101_LSTM_DATA, "train")
+        self.test_dir = os.path.join(constants.UCF_101_LSTM_DATA, "test")
 
     def get_train_data(self):
         return SequenceDataGenerator(self.batch_size, self.train_dir, self.config.exp.num_of_classes)
