42e0e790f23f0f09eb344e57c9099b26e519d8bb
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-04 16:03:41 +0200

    Added configs and script for training LSTM

---
+++ train_lstm.sh
@@ -0,0 +1,17 @@
+!#/bin/bash
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_1.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_2.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_3.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_4.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_5.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_6.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_7.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_8.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_9.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_10.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_11.json
+python3 main.py --memory_frac 0.9 --config custom_configs/lstm_config/config_12.json
+python3 main.py --memory_frac 0.9 --config custom_configs/fine_tune_configs/config_11.json
+python3 main.py --memory_frac 0.9 --config custom_configs/fine_tune_configs/config_12.json
+python3 main.py --memory_frac 0.9 --config custom_configs/fine_tune_configs/config_13.json
+python3 main.py --memory_frac 0.9 --config custom_configs/fine_tune_configs/config_14.json
