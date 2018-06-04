0b860e268487a63b4298a3b02fbd5c9def42ef60
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-04 12:32:32 +0200

    Added configs for fine tuned model and script which runs all of them

---
+++ train_fine_tuned.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_1.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_2.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_3.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_4.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_5.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_6.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_7.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_8.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_9.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_10.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_11.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_12.json
