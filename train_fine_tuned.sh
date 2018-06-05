eb928bd133169015e0621ffa97cf2b72f932ca16
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-05 16:16:19 +0200

    Changed script for trianing

--- train_fine_tuned.sh
+++ train_fine_tuned.sh
@@ -5,9 +5,7 @@ python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/conf
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_4.json
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_5.json
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_6.json
-python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_7.json
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_8.json
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_9.json
 python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_10.json
-python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_11.json
-python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_12.json
+python3 main.py --memory_frac 0.8 --config custom_configs/fine_tune_configs/config_13.json
