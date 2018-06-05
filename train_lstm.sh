efe676631b60792265c635a7fd4186c97ae338d5
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-05 09:32:45 +0200

    Added more cases

--- train_lstm.sh
+++ train_lstm.sh
@@ -8,3 +8,7 @@ python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_18
 python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_19.json
 python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_20.json
 python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_21.json
+python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_22.json
+python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_23.json
+python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_24.json
+python3 main.py --memory_frac 0.5 --config custom_configs/lstm_configs/config_25.json
