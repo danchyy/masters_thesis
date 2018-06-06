3928b29e6e7546a6abbd76cb8c67b48f4e2ac2f3
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-06 16:47:33 +0200

    Added more stuff to config

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -1,11 +1,9 @@
 from base.base_model import BaseModel
-from keras.models import Sequential
 from keras.layers.recurrent import LSTM
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, AveragePooling1D, Flatten
 from keras.models import Model
 from keras import optimizers, Input
 from utils import constants
-from utils.util_script import get_number_of_classes
 
 
 class LSTMModel(BaseModel):
@@ -20,18 +18,27 @@ class LSTMModel(BaseModel):
             for i in range(len(self.config.model.architecture.lstm)):
                 neurons = self.config.model.architecture.lstm[i]
                 dropout = self.config.model.architecture.dropout[i]
-                return_sequences = i != (len(self.config.model.architecture.lstm)-1)
+                return_sequences = True if i < (len(self.config.model.architecture.lstm)-1) \
+                    else self.config.model.unrolled_sequences
                 if i == 0:
                     x = LSTM(neurons, return_sequences=return_sequences, dropout=dropout)(input)
                 else:
                     x = LSTM(neurons, return_sequences=return_sequences, dropout=dropout)(x)
-            for neurons in self.config.model.architecture.dense:
+            for i in range(len(self.config.model.architecture.dense)):
+                neurons, dropout_rate = self.config.model.architecture.dense[i], \
+                                        self.config.model.architecture.dense_dropout[i]
                 x = Dense(neurons, activation="relu")(x)
+                x = Dropout(dropout_rate)(x)
         else:
             x = LSTM(1024, return_sequences=False, dropout=0.5)(input)
             x = Dense(512, activation="relu")(x)
             x = Dropout(0.5)(x)
+
+        # classifier
         predictions = Dense(self.config.exp.num_of_classes, activation="softmax")(x)
+        if self.config.model.unrolled_sequences:
+            predictions = AveragePooling1D(pool_size=constants.LSTM_SEQUENCE_LENGTH)(predictions)
+            predictions = Flatten()(predictions)
         self.model = Model(inputs=input, outputs=predictions)
 
         optimizer = optimizers.get(self.config.model.optimizing.optimizer)
