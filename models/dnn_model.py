e20db3d7558f1368ddf2fa9691b4fc71f2a35d20
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-06 15:13:59 +0200

    Another attempt with DNN

--- models/dnn_model.py
+++ models/dnn_model.py
@@ -1,6 +1,5 @@
 from base.base_model import BaseModel
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, TimeDistributed, AveragePooling1D
 from keras import optimizers, Input
 from utils import constants
 from keras.models import Model
@@ -13,21 +12,25 @@ class DNNModel(BaseModel):
 
     def build_model(self):
         input = Input(shape=(constants.LSTM_SEQUENCE_LENGTH, constants.LSTM_FEATURE_SIZE))
-        x = Flatten()(input)
         if self.config.model.architecture.available:
+            first = True
             for layer_num in self.config.model.architecture.dense:
-                x = Dense(layer_num, activation="relu")(x)
-                x = Dropout(0.5)(x)
+                if first:
+                    x = TimeDistributed(Dense(layer_num, activation="relu"))(input)
+                    first = False
+                else:
+                    x = TimeDistributed(Dense(layer_num, activation="relu"))(x)
+                x = TimeDistributed(Dropout(0.5))(x)
         else:
-            x = Dense(1024, activation="relu")(x)
-            x = Dropout(0.5)(x)
-            x = Dense(1024, activation="relu")(x)
-            x = Dropout(0.5)(x)
-            x = Dense(1024, activation="relu")(x)
-            x = Dropout(0.5)(x)
-        predictions = Dense(self.config.exp.num_of_classes, activation="softmax")(x)
+            x = TimeDistributed(Dense(1024, activation="relu"))(input)
+            x = TimeDistributed(Dropout(0.5))(x)
+            x = TimeDistributed(Dense(1024, activation="relu"))(x)
+            x = TimeDistributed(Dropout(0.5))(x)
+        x = Dense(self.config.exp.num_of_classes, activation="softmax")(x)
+        predictions = AveragePooling1D(pool_size=constants.LSTM_SEQUENCE_LENGTH)(x)
+        predictions = Flatten()(predictions)
         self.model = Model(inputs=input, outputs=predictions)
-
+        print(self.model.output.shape)
         optimizer = optimizers.get(self.config.model.optimizing.optimizer)
         assert isinstance(optimizer, optimizers.Optimizer)
         optimizer.lr = self.config.model.optimizing.learning_rate
