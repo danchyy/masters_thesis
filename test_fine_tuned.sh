dccdab62a5511745ec8fb5f3649b72bed77e307c
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-15 00:45:13 +0200

    Added configs

---
+++ test_fine_tuned.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_1_1.json
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_1_2.json
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_1_3.json
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_2_1.json
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_2_2.json
+python3 main.py --config custom_configs/testing/fine_tune_configs/config_2_3.json
