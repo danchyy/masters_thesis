5fb3861a2db02faf9446edad8ea13b0cf46669e6
Daniel Bratulić <danielbratulic@yahoo.com>
2018-06-19 09:46:47 +0200

    fix

--- extract_data_for_cnn.sh
+++ extract_data_for_cnn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 utils/prepare_data_for_cnn.py train_val01.txt test01.txt
 python3 utils/prepare_data_for_cnn.py train_val02.txt test02.txt
-python3 utils/prepare_data_for_cnn.py train_val02.txt test03.txt
+python3 utils/prepare_data_for_cnn.py train_val03.txt test03.txt
 python3 main.py --memory_frac 0.7 --config custom_configs/testing/fine_tune_configs/config_1_1.json
 python3 main.py --memory_frac 0.7 --config custom_configs/testing/fine_tune_configs/config_1_2.json
 python3 main.py --memory_frac 0.7 --config custom_configs/testing/fine_tune_configs/config_1_3.json
